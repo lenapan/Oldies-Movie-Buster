8e6a933885002fd91cb1a288d0eef3eee95f46ab
Lena Pan <lenapan.tech@gmail.com>
2021-10-04 17:38:32 -0700

    Basic scaffolding for Oldie's Movie Buster

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
+import Header from "./components/Header/index.js";
+import Main from "./components/Main/index.js";
+import Footer from "./components/Footer/index.js";
 
+import "./styles/style.css"
 
-function App() {
-  // return <  />;
+class App extends Component {
+   render() {
+    return (
+      <>
+        <div className="">
+          <Header/> 
+          <Main/>
+          <Footer year={new Date().getFullYear()}/>
+        </div>
+      </>
+      );
+   }
 }
-
 export default App;
